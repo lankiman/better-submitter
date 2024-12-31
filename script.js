102ec28172e422a3e795aea955ec2f0a0eeab9ba
Lankiman <marvellousgboun@gmail.com>
2024-12-31 21:05:48 +0100

    making last changes for the year

--- script.js
+++ script.js
@@ -759,8 +759,8 @@ handleChooseCourseStep.init();
 
 //Handle Files Selection Containier
 
-const selectedCodeFilesState = createStateManager([]);
-const selectedVideoFilesState = createStateManager([]);
+const selectedCodeFilesState = createStateManager(new Map());
+const selectedVideoFilesState = createStateManager(new Map());
 
 const handleFileSelection = (() => {
   const uploadChoiceState = createStateManager("code");
@@ -909,43 +909,185 @@ const handleFileSelection = (() => {
     }
   }
 
-  function updateFileListItem(file) {
-    const fileChoice = uploadChoiceState.getState();
-  }
-
   function handleFileSelection(files) {
     const fileChoice = uploadChoiceState.getState();
     if (fileChoice == "code") {
       const selectedCodeFiles = selectedCodeFilesState.getState();
       for (let file of files) {
-        const existingIndex = selectedCodeFiles.findIndex(
-          (existingFile) => existingFile === file.name
-        );
-        if (existingIndex !== 1) {
-          selectedCodeFiles.splice(existingIndex, 1, file);
-          selectedCodeFilesState.setState(selectedCodeFiles);
+        if (selectedCodeFiles.has(file.name)) {
+          selectedCodeFiles[file.name] = {
+            assignmentFile: file,
+            assignmentNumber: null
+          };
+          updateFileListItem(file.name);
         } else {
-          selectedCodeFiles.push(file);
-          selectedCodeFilesState.setState(selectedCodeFiles);
+          selectedCodeFiles.set(file.name, {
+            assignmentFile: file,
+            assignmentNumber: null
+          });
+          addFileToList(file);
         }
+        selectedCodeFiles.setState(selectedCodeFiles);
       }
+      // for (let file of files) {
+      //   const existingIndex = selectedCodeFiles.findIndex(
+      //     (existingFile) => existingFile === file.name
+      //   );
+      //   if (existingIndex !== 1) {
+      //     selectedCodeFiles.splice(existingIndex, 1, file);
+      //     selectedCodeFilesState.setState(selectedCodeFiles);
+      //   } else {
+      //     selectedCodeFiles.push(file);
+      //     selectedCodeFilesState.setState(selectedCodeFiles);
+      //   }
+      // }
       codeFilesInput.value = "";
     } else {
       const selectedVideoFiles = selectedVideoFilesState.getState();
+      // for (let file of files) {
+      //   const existingIndex = selectedVideoFiles.findIndex(
+      //     (existingFile) => existingFile === file.name
+      //   );
+      //   if (existingIndex !== 1) {
+      //     selectedVideoFiles.splice(existingIndex, 1, file);
+      //     selectedVideoFilesState.setState(selectedVideoFiles);
+      //   } else {
+      //     selectedVideoFiles.push(file);
+      //     selectedVideoFilesState.setState(selectedVideoFiles);
+      //   }
+      // }
       for (let file of files) {
-        const existingIndex = selectedVideoFiles.findIndex(
-          (existingFile) => existingFile === file.name
-        );
-        if (existingIndex !== 1) {
-          selectedVideoFiles.splice(existingIndex, 1, file);
-          selectedVideoFilesState.setState(selectedVideoFiles);
+        if (selectedVideoFiles.has(file.name)) {
+          selectedVideoFiles[file.name] = {
+            assignmentFile: file,
+            assignmentNumber: null
+          };
+          updateFileListItem(file.name);
         } else {
-          selectedVideoFiles.push(file);
-          selectedVideoFilesState.setState(selectedVideoFiles);
+          selectedVideoFiles.set(file.name, {
+            assignmentFile: file,
+            assignmentNumber: null
+          });
+          addFileToList(file);
         }
+        selectedVideoFiles.setState(selectedVideoFiles);
       }
       videoFilesInput.value = "";
     }
+  }
+
+  function createFilelistItem(file) {
+    const fileChoice = uploadChoiceState.getState();
+    const codeFileSvg = `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+            />
+          </svg>`;
+    const videoFileSvg = `<svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="size-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0 1 18 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0 1 18 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 0 1 6 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5"
+      />
+    </svg>`;
+    const li = document.createElement("li");
+    li.classList.add("selected-file-list-item");
+    li.setAttribute("data-selected-files-list-item", true);
+    li.innerHTML = `
+        <div class="selected-file-list-item-svg">
+         ${fileChoice === "code" ? codeFileSvg : videoFileSvg}
+        </div>
+        <div class="selected-file-list-item-details">
+          <p
+            data-selected-file-list-item-name
+            class="selected-file-list-item-name"
+          >
+            test file name
+          </p>
+          <div class="selected-file-list-buttons-section">
+            <button
+              class="selected-file-list-button select-assignment-number-button"
+              data-select-assignment-number-button
+            ></button>
+            <button
+              class="delete-selected-file-button selected-file-list-button"
+              data-delete-selected-file-button
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="3"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M5 12h14"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>`;
+
+    const deleteFileButton = li.querySelector(
+      "[data-slelected-file-delete-button]"
+    );
+    deleteFileButton.addEventListener("click", () => {
+      removeSelectedFile(file.name);
+    });
+
+    const selectAssignmentNumberButton = li.querySelector(
+      "[data-select-assignment-number-button]"
+    );
+
+    return li;
+  }
+
+  function addFileToList(file) {
+    const fileChoice = uploadChoiceState.getState();
+    const li = createFilelistItem(file);
+    fileChoice === "code"
+      ? selelecedCodeFilesList.appendChild(li)
+      : selectedVideoFilesList.appendChild(li);
+  }
+
+  function updateFileListItem(fileName) {
+    const fileChoice = uploadChoiceState.getState();
+    const listItems = null;
+
+    fileChoice === "code"
+      ? (listItems = selelecedCodeFilesList.querySelectorAll(
+          "[data-selected-files-list-item]"
+        ))
+      : (listItems = selectedVideoFilesList.querySelectorAll(
+          "[data-selected-files-list-item]"
+        ));
+
+    listItems.forEach((item) => {
+      const fileNameElement = item.querySelector("[data-selected-file-name]");
+
+      if (fileNameElement.textContent === fileName) {
+        fileNameElement.textContent = fileName;
+      }
+    });
   }
 
   function initializeSubscribers() {
